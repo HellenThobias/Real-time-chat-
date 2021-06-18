1f946cd50d497094181e3cdaaf1f914e84543e59
HellenTobhias <mgenih914@gmail.com>
2021-06-18 18:16:04 +0300

    changing and deleting files

--- src/ChatServerPlayground/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path
-from personal.views import home
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home, name='home')
-]
--- src/personal/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def home(request, *args, **kwargs):
-    context={}
-    return render(request, 'personal/home.html', context)
