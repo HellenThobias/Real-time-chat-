8f223ff1d503eb4a29f833e3959273193f66b851
HellenTobhias <mgenih914@gmail.com>
2021-08-08 15:17:14 +0300

    cancelling friend request

--- friends/urls.py
+++ friends/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path 
 
-from friends.views import send_friend_request, friend_requests_view, accept_friend_request,remove_friend, decline_friend_request
+from friends.views import send_friend_request, friend_requests_view, accept_friend_request,remove_friend, decline_friend_request,cancel_friend_request
 
 app_name="friends"
 
 urlpatterns = [
     path('friend_remove/', remove_friend, name="remove-friend"),
     path('friend_request/', send_friend_request, name="friend-request"),
+    path('friend_request_cancel/', cancel_friend_request, name="friend-request-cancel"),
     path('friend_request/<user_id>/', friend_requests_view, name="friend-requests"),
     path('accept_friend_request/<friend_request_id>/', accept_friend_request, name="friend-request-accept"),
     path('decline_friend_request/<friend_request_id>/', decline_friend_request, name="friend-request-decline"),
